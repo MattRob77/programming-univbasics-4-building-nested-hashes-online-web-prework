43c4a051e7699223ad36f60634a83a0ad8ed4ec3
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-24 15:39:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ end
 def monopoly_with_fourth_tier
   monopoly = {
     railroads: {
-      :names=>{:pennsylvania_railroad => {}, :reading_railroad =>{}, :b_and_o_railroad => {}, :shortline_railroad => {}},
+      :names=>{:pennsylvania_railroad => {}, :reading_railroad =>{:mortgage}, :b_and_o_railroad => {}, :shortline_railroad => {}},
       pieces: 4,
       rent_in_dollars: {
         :one_piece_owned=>25,

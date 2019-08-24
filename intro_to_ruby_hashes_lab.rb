40510e58097ae358e3ac95e2fe7f02e52ef544ca
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-24 15:32:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
 def monopoly_with_third_tier
 monopoly = {
     railroads: {
-      :names=>{:pennsylvania_railroad => {}, :reading_railroad => {}[], :b_and_o_railroad => {}, :shortline_railroad => {}},
+      :names=>{:pennsylvania_railroad => {}, :reading_railroad => {}[ "mortgage_value"=>"$100"], :b_and_o_railroad => {}, :shortline_railroad => {}},
       pieces: 4,
       rent_in_dollars: {
         :one_piece_owned=>25,
